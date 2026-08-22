a13590b1fee3cd29be0d76c0e083228d97d8bab5
teamvenkat <venkatdurgempudi@gmail.com>
2026-08-22 18:54:02 +0530

    Stop tracking dbt target files

--- target/compiled/gcp_hospital_medallion/models/schema.yml/updated_at_not_before_created__fde1b4cb93ccf50b5534ef75dbdd8d0c.sql
+++
@@ -1,6 +0,0 @@
-
-
-SELECT *
-FROM `project-5fbc8bf7-2dd6-4f0a-a5f`.`hospital_silver`.`stg_departments`
-WHERE updated_at < created_at
-
--- target/compiled/gcp_hospital_medallion/tests/discharge_after_admission.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    d.discharge_id,
-    d.admission_id,
-    d.discharge_date,
-    a.admission_date
-FROM `project-5fbc8bf7-2dd6-4f0a-a5f`.`hospital_silver`.`stg_discharges` AS d
-JOIN `project-5fbc8bf7-2dd6-4f0a-a5f`.`hospital_silver`.`stg_admissions` AS a
-    ON d.admission_id = a.admission_id
-WHERE d.discharge_date < a.admission_date
